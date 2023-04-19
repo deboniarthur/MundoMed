a970b17cff8bfeb696e3526384bb80f686b26003
Arthur Guilherme do Nascimento Deboni <123404812+deboniarthur@users.noreply.github.com>
2023-04-19 12:04:13 -0300

    Commit Inicial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Alunos: Arthur Guilherme do Nascimento Deboni e Victor de Oliveira Leiroz
+// Matrícula Arthur:1230105304
+// Matrícula Victor:1230114264
+// Turma A (Segunda e Quinta)
+
+// Mostrando a data na nav-bar usando JS
+
+data = new Date
+monName = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+document.write("Dia " + data.getDate() + " de " + monName[data.getMonth()] + " de " + data.getFullYear());
+
+
+
